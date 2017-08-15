29561097f093a3252cb31dec66325eae93371b82
harshit <tailang92@gmail.com>
2017-08-15 21:33:44 +0530

    2nd commit. partial sprint1_1

--- quickstart/src/app/app.component.ts
+++ quickstart/src/app/app.component.ts
@@ -2,6 +2,35 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: 
+            ` <h1>{{title}}</h1> 
+              <h2>{{hero.name}} Details!
+              <div> <label>Id: </label>{{hero.id}} </div>
+              <div> <label>Name: </label><input [(ngModel)]="hero.name" placeholder="name"> </div>
+             `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+  name = 'Angular'; 
+  title = 'Tour of Heroes';
+  
+  hero: Hero = {id : 1, name : 'Windstorm'};
+  heroes = HEROES;
+}
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
+export class Hero {
+  id: number;
+  name: string;
+}
